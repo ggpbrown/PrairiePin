00d2ae1672032e11ea3d8282bd77da4642b3e497
Grant Brown <grantbrown@mac.home>
2025-05-09 05:14:12 -0400

    Replace bcrypt with bcriptjs for Railway

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,6 @@
 // server/auth.js
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Pool } = require('pg');
 require('dotenv').config();
