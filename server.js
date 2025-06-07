cf65507e9c26ff5e80984dee9b8f7732e3a2d573
Grant Brown <ggpbrown@gmail.com>
2025-06-07 06:27:48 -0400

    Fix: add fields to sql codes retrieving data for editing profile

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/my-profile', authenticateToken, async (req, res) => {
     console.log("🔐 Access granted to /my-profile for user:", req.user?.userId);
     const userId = req.user.userId;
     const result = await pool.query(
-      'SELECT first_name, last_name, email FROM users WHERE id = $1',
+      'SELECT first_name, last_name, email, city, province_state FROM users WHERE id = $1',
       [userId]
     );
 
