a16ec37802d46b92f06bb94c22c8165967e14c63
Grant Brown <ggpbrown@gmail.com>
2025-06-22 17:58:16 -0400

    Edit: add console logging for admin-dashboard route

--- server.js
+++ server.js
@@ -282,9 +282,6 @@ app.get('/profile', authenticateToken, (req, res) => {
   res.sendFile(path.join(__dirname, 'public/user-profile.html'));
 });
 
-app.get('/admin-dashboard', authenticateToken, async (req, res) => {
-  res.render('admin-dashboard', { user: req.user });
-});
 
 // 🚦 Start Server
 app.listen(PORT, () => {
